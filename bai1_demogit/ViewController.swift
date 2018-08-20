81836019ee6ec0eb3cf69284537d0ad48e4ce209
Huy Duc <huyparody@nguyens-mac-mini.local>
2018-08-20 08:51:23 +0700

    Initial Commit

---
+++ bai1_demogit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bai1_demogit
+//
+//  Created by Huy Trinh Duc on 8/20/18.
+//  Copyright © 2018 Huy Trinh Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
